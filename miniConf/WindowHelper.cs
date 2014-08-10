26098f18745904b22d5f82f520eabfe4a1243fbe
Max Weller <git@max-weller.de>
2014-08-10 10:00:30 +0200

    Single instance mutex; Answer to Ping IQs; respect comments in room list

--- miniConf/WindowHelper.cs
+++ miniConf/WindowHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
@@ -9,7 +10,14 @@ namespace miniConf
     class WindowHelper
     {
         [System.Runtime.InteropServices.DllImport("user32.dll")]
-        static extern IntPtr GetForegroundWindow();
+        public static extern IntPtr GetForegroundWindow();
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+        [DllImport("user32")]
+        public static extern int RegisterWindowMessage(string message);
+
+        public const int HWND_BROADCAST = 0xffff;
+        public static readonly int WM_SHOWME = RegisterWindowMessage("WM_SHOWME");
 
         public static bool IsActive(Control wnd)
         {
