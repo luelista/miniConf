ac69b4ca4fa62bef165c4bcb63d0e197eaa97916
Max Weller <git@max-weller.de>
2015-04-26 18:54:32 +0200

    extract selection of messageView implementation to own method

--- miniConf/VbHelper.cs
+++ miniConf/VbHelper.cs
@@ -9,6 +9,21 @@ namespace miniConf {
 		private VbHelper () {
 		}
 
+
+        public static IMessageView createMessageView() {
+            if (!runningOnMono()) {
+                MessageView browser = new MessageView ();
+                browser.imagePreview = true;
+                browser.Dock = DockStyle.Fill;
+                //browser.Url = new System.Uri("about:blank", System.UriKind.Absolute);
+                return browser;
+            } else {
+                MessageViewStub stub = new MessageViewStub ();
+                stub.Dock = DockStyle.Fill;
+                return stub;
+            }
+        }
+
 		public static bool runningOnMono() {
 			return Type.GetType ("Mono.Runtime") != null;
 		}
